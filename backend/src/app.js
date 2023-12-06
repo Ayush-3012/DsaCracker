9155632f7ceac761ae95c5f07b45cf3e80df1d60
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-12-07 01:23:21 +0530

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,7 @@ import cors from "cors";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["*"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
