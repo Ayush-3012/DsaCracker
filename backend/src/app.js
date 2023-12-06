5020735f29614b7e9ff27ff2087f8b77ed146bbe
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-12-07 00:49:06 +0530

    fixing cors problem

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,19 +4,7 @@ import cors from "cors";
 
 const app = express();
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // All origins
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  ); // All HTTP methods
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  ); // All headers
-  next();
-});
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
