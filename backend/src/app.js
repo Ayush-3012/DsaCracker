7806f99e17a9d0968276edc4ae55ce99327850fd
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-12-07 01:05:11 +0530

    nhi ho rha resolve ye cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,19 +4,7 @@ import cors from "cors";
 
 const app = express();
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
