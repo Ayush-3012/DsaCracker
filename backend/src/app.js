df9842d20621c8c6cf3d5bfb62fb66fa4d542c95
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-12-07 01:10:52 +0530

    final try for today

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,13 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
